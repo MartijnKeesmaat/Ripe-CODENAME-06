2c6d8dab847d39c86ec917b5fa316936af6618bc
Martijn <mpkeesmaat@gmail.com>
2020-04-28 20:43:05 +0200

    typeface

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,7 +22,7 @@ function pageTransition() {
 
 function contentAnimation() {
   gsap.from('main', {
-    duration: 0.2,
+    duration: 0.4,
     y: 30,
     autoAlpha: 0,
     delay: 0.5,
@@ -102,11 +102,15 @@ function slide(from, to) {
     ease: 'power4.inOut',
   });
 
-  heroTl.to(to, {
-    autoAlpha: 1,
-    duration: 0.5,
-    ease: 'power1.inOut',
-  });
+  heroTl.to(
+    to,
+    {
+      autoAlpha: 1,
+      duration: 0.5,
+      ease: 'power1.inOut',
+    },
+    '-=0.5'
+  );
 
   heroTl.to(
     '.slide-progress__current',
@@ -143,3 +147,121 @@ function showNextTab() {
 //   duration: 0.3,
 //   delay: 1,
 // });
+
+let descriptionTl = gsap.timeline();
+
+descriptionTl.to('.slide__description span:nth-of-type(1)', {
+  autoAlpha: 0,
+  y: -30,
+  delay: 3.5,
+});
+
+descriptionTl.to('.slide__description span:nth-of-type(2)', {
+  autoAlpha: 1,
+  y: 0,
+});
+
+descriptionTl.to('.slide__description span:nth-of-type(2)', {
+  autoAlpha: 0,
+  y: -30,
+  delay: 8,
+});
+
+descriptionTl.to('.slide__description span:nth-of-type(3)', {
+  autoAlpha: 1,
+  y: 0,
+});
+
+// descriptionTl.pause();
+
+let detailTimeline = gsap.timeline();
+detailTimeline.pause();
+
+detailTimeline.to('.tabs', {
+  autoAlpha: 0,
+  delay: 1,
+  duration: 0.3,
+});
+
+detailTimeline.to(
+  '.slide__description',
+  {
+    autoAlpha: 0,
+    duration: 0.3,
+  },
+  '-=0.3'
+);
+
+detailTimeline.to(
+  '.slide__arrows',
+  {
+    autoAlpha: 0,
+    duration: 0.3,
+  },
+  '-=0.3'
+);
+
+detailTimeline.to('.hero', {
+  x: -170,
+  scale: 0.88,
+  y: 40,
+  duration: 0.6,
+  ease: 'power2.out',
+});
+
+detailTimeline.to('.slide-detail', {
+  autoAlpha: 1,
+  y: 0,
+  duration: 0.6,
+  ease: 'power2.out',
+});
+
+detailTimeline.to('.slide-detail', {
+  autoAlpha: 0,
+  y: -30,
+  duration: 0.6,
+  ease: 'power2.out',
+  delay: 2,
+});
+
+detailTimeline.to(
+  '.fade',
+  {
+    y: '-100%',
+    duration: 1.5,
+    stagger: 0.2,
+  },
+  '-=0.4'
+);
+
+detailTimeline.to(
+  '.grid__item img',
+  {
+    y: 0,
+    duration: 1,
+    stagger: 0.3,
+    ease: 'power2.out',
+  },
+  '-=2'
+);
+
+const cta = document.querySelector('.slide__cta');
+cta.addEventListener('click', function () {
+  detailTimeline.play();
+});
+
+const ham = document.querySelector('.hamburger');
+ham.addEventListener('click', function (e) {
+  e.currentTarget.classList.add('hamburger--active');
+  gsap.to('.menu-overlay', {
+    autoAlpha: 1,
+    duration: 1,
+  });
+  gsap.to('.menu-overlay img', {
+    autoAlpha: 1,
+    duration: 1,
+    x: 0,
+    stagger: 0.2,
+    delay: 1,
+  });
+});
