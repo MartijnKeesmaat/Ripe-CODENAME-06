20e12d51f4f216cb43fd6275a4a01ae732850a83
Martijn <mpkeesmaat@gmail.com>
2020-04-28 16:23:34 +0200

    slider-homepage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -66,3 +66,80 @@ barba.init({
     },
   ],
 });
+
+// SLIDER
+const tabButtons = document.querySelectorAll('.tab-button');
+// tabButtons.forEach((e) => e.addEventListener('click', newTab));
+
+function newTab(e) {
+  e.currentTarget.classList.add('tab-button--active');
+}
+
+var heroTl = gsap.timeline();
+let progress = 0;
+
+function slide(from, to) {
+  const step = 115;
+  progress = progress - step;
+
+  heroTl.to('.slide-progress__current', {
+    width: '100%',
+    duration: 3,
+    ease: 'power1.inOut',
+  });
+
+  heroTl.to(from, {
+    autoAlpha: 0,
+    duration: 0.5,
+    ease: 'power1.inOut',
+    onComplete: showNextTab,
+  });
+
+  heroTl.to('.hero-image', {
+    x: `${progress}%`,
+    stagger: 0.4,
+    duration: 1.2,
+    ease: 'power4.inOut',
+  });
+
+  heroTl.to(to, {
+    autoAlpha: 1,
+    duration: 0.5,
+    ease: 'power1.inOut',
+  });
+
+  heroTl.to(
+    '.slide-progress__current',
+    {
+      width: '0%',
+      duration: 0.3,
+      ease: 'power1.inOut',
+    },
+    '-=1.7'
+  );
+
+  // heroTl.
+  // .classList.add('tab-button--active');
+
+  heroTl.to('.slide-progress__current', {
+    width: '100%',
+    duration: 3,
+    ease: 'power1.inOut',
+  });
+}
+
+slide('.hero-letter:nth-of-type(1)', '.hero-letter:nth-of-type(2)');
+slide('.hero-letter:nth-of-type(2)', '.hero-letter:nth-of-type(3)');
+
+let currentTab = 0;
+function showNextTab() {
+  currentTab++;
+  tabButtons.forEach((e) => e.classList.remove('tab-button--active'));
+  tabButtons[currentTab].classList.add('tab-button--active');
+}
+
+// gsap.to('.tab-button:first-of-type', {
+//   borderWidth: 0,
+//   duration: 0.3,
+//   delay: 1,
+// });
